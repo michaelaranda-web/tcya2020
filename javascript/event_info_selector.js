376bad194684496190d621e82c4af29750112ebb
EC2 Default User <ec2-user@ip-172-31-36-18.us-east-2.compute.internal>
2020-06-20 22:46:30 +0000

    Adds Edge-specific styling and some minor miscellaneous style tweaks.

--- javascript/event_info_selector.js
+++ javascript/event_info_selector.js
@@ -9,16 +9,16 @@ function EventInfoSelector() {
     if (sectionName === "general") {
       this.selectedSection = "general";
       
-      $( "#schedule-button" ).parent().removeClass("selected");
-      $( "#general-info-button" ).parent().addClass("selected");
+      $( "#schedule-button" ).removeClass("selected");
+      $( "#general-info-button" ).addClass("selected");
       
       $( "#schedule-section" ).hide();
       $( "#general-info-section" ).show( 'drop', {}, 500 );
     } else {
       this.selectedSection = "schedule";
       
-      $( "#general-info-button" ).parent().removeClass("selected");
-      $( "#schedule-button" ).parent().addClass("selected");
+      $( "#general-info-button" ).removeClass("selected");
+      $( "#schedule-button" ).addClass("selected");
       
       $( "#general-info-section" ).hide();
       $( "#schedule-section" ).show( 'drop', {}, 500 );
